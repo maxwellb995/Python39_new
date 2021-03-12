f670e64d49f0bf143b79b35c631f012b46744d88
Maxwell Ramirez <maxwell.b.park.ramirez@gmail.com>
2021-03-12 03:15:30 -0500

    new loop

--- python39_new.py
+++ python39_new.py
@@ -4,7 +4,6 @@
 
 
 if __name__ == '__main__':
-    print("hello python39_new")
     print("I hope this makes it into GitHub")
     print("I hope this makes it into GitHub2")
     print("I hope this makes it into GitHub3")
@@ -16,3 +15,4 @@ if __name__ == '__main__':
     for x in my_list:
         print(x)
 
+    print("hello python39_new")
